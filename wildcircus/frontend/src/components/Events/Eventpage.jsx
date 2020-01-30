77dbcb87e614af554e26ae6b119073c825ad5742
Superdref <frederiquemendy2205@gmail.com>
2020-01-30 16:47:33 +0100

    More css in frontend

--- wildcircus/frontend/src/components/Events/Eventpage.jsx
+++ wildcircus/frontend/src/components/Events/Eventpage.jsx
@@ -1,13 +1,69 @@
 import React from "react";
-//import { Link } from "ract-router-dom";
-//import styles from "./Eventpage.module.css";
+import { Link } from "react-router-dom";
+import styles from "./Eventpage.module.css";
 
-//const {} = styles;
+import { Image, Menu } from "semantic-ui-react";
+import Logo from "../../assets/logo wc.png";
+import Troubadours from "../../assets/street-style.jpg";
+import Burlesque from "../../assets/burlesque-style.jpg";
+import Motor from "../../assets/motor-bubble.jpg";
+import Water from "../../assets/pool-show.jpg";
+
+const { logowildcircus, menu, section, fool, fun, speed, aqua } = styles;
 
 function Eventpage() {
   return (
     <>
-      <div></div>
+      <div>
+        <navbar>
+          <Menu.Item className={menu}>
+            <Link to="./" className={section}>
+              About Us
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./Events" className={section}>
+              Events
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./Gallery" className={section}>
+              Gallery
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./Booking" className={section}>
+              Booking
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./" className={section}>
+              Contact Us
+            </Link>
+          </Menu.Item>
+        </navbar>
+        <Image src={Logo} className={logowildcircus} alt="Logo Wild Circus" />
+        <h2>Art is everywhere...</h2>
+        <h3>The Street Show</h3>
+        <Image src={Troubadours} className={fool} alt="The Street Band" />
+        <h3>The Burlesque Show</h3>
+        <Image src={Burlesque} className={fun} alt="The Funny Band" />
+        <h3>The Motor Show</h3>
+        <Image src={Motor} className={speed} alt="The Motor Bubble" />
+        <h3>The Pool Show</h3>
+        <Image src={Water} className={aqua} alt="The Mermaids' Band" />
+
+        <article>
+          Our circus is realy a new vision of the street arts mixed with the
+          original circus world... more we believe in respect for wildlife, so
+          the animals that play with us are fake!!! Also we love music and
+          that's why we often have guests as singer or Djs.
+        </article>
+        <Link to="./Events">
+          <h2>Let us present the artists...</h2>
+        </Link>
+        <footer></footer>
+      </div>
     </>
   );
 }
--- wildcircus/frontend/src/components/Homepage/Homepage.jsx
+++ wildcircus/frontend/src/components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Image } from "semantic-ui-react";
+import { Image, Menu } from "semantic-ui-react";
 import Logo from "../../assets/logo wc.png";
 import NewBuffon from "../../assets/modern-buffon.jpg";
 import Buffon from "../../assets/saltimbanque.jpg";
@@ -8,20 +8,46 @@ import Juggler from "../../assets/street-juggler.jpg";
 import Statue from "../../assets/street-statue.jpg";
 import styles from "./Homepage.module.css";
 
-const { logowildcircus, buffon, oldbuffon, juggler, statue } = styles;
+const {
+  logowildcircus,
+  menu,
+  section,
+  buffon,
+  oldbuffon,
+  juggler,
+  statue
+} = styles;
 
 function Homepage() {
   return (
     <>
       <div>
         <navbar>
-          <ul>
-            <li>About Us</li>
-            <li>Events</li>
-            <li>Gallery</li>
-            <li>Booking</li>
-            <li>Contact Us</li>
-          </ul>
+          <Menu.Item className={menu}>
+            <Link to="./" className={section}>
+              About Us
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./Events" className={section}>
+              Events
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./Gallery" className={section}>
+              Gallery
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./Booking" className={section}>
+              Booking
+            </Link>
+          </Menu.Item>
+          <Menu.Item className={menu}>
+            <Link to="./" className={section}>
+              Contact Us
+            </Link>
+          </Menu.Item>
         </navbar>
         <Image src={Logo} className={logowildcircus} alt="Logo Wild Circus" />
         <h2>Art is everywhere...</h2>
