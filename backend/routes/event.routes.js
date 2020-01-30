c2e63a147f225a3e7d1c390aa42716f79ad31f5e
Superdref <frederiquemendy2205@gmail.com>
2020-01-30 09:48:30 +0100

    Create artist and event routes in backend

--- backend/routes/event.routes.js
+++ backend/routes/event.routes.js
@@ -7,3 +7,26 @@ const router = express.Router();
 const Event = require("../sequelize/models/events");
 const User = require("../sequelize/models/users");
 const Artist = require("../sequelize/models/artists");
+
+router.get("/", (req, res) => {
+  Event.findAll()
+    .then(events => res.status(200).json(events))
+    .catch(err => res.status(400).json(err));
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Event.findOne({
+    where: {
+      uuid: id
+    }
+  })
+    .then(events => {
+      res.status(200).json(events);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
